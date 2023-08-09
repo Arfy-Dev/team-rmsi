f48c3b67ce5202239d83a68dae27d24858f41c3e
Ahmad-SIF21 <12150311729@students.uin-suska.ac.id>
2023-08-09 22:42:10 +0700

    Update righSidebar_template.php

--- settings/Block Plugins/righSidebar_template.php
+++ settings/Block Plugins/righSidebar_template.php
@@ -1,7 +1,4 @@
-<!-- Right Sidebar - Template -->
 <span class="blockTitle">Template</span>
 <a class="template" href="https://s.id/1CsBw" target="_blank">
-    <img src="http://journal.stisipolmujahidin.ac.id/public/site/images/admin/doc-template.png" alt="Template"
-        class="imgTemplate" width="170" height="55">
+    <img src="https://s.id/1Shth" alt="Article Template" class="imgTemplate" width="175" height="55">
 </a>
-<!-- Right Sidebar - Template -->
