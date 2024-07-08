e36a40dd47496dbf30d91d866636befdb77aba0b
Arfy-Dev <12150311729@students.uin-suska.ac.id>
2024-07-08 20:48:16 +0700

    update 8/7/24
    Update Publication Fee

--- settings/Block Plugins/righSidebar_template.php
+++ settings/Block Plugins/righSidebar_template.php
@@ -1,4 +1,4 @@
 <span class="blockTitle">Template</span>
 <a class="template" href="https://s.id/1CsBw" target="_blank">
-    <img src="https://s.id/1Shth" alt="Article Template" class="imgTemplate" width="175" height="55">
+    <img src="/public/site/images/saide/ArticleTemplate.png" alt="Article Template" class="imgTemplate" width="175" height="55">
 </a>
--- settings/Block Plugins/rightSidebar_acreditation.php
+++ settings/Block Plugins/rightSidebar_acreditation.php
@@ -1,7 +1,7 @@
 <!-- Right Sidebar - Acreditation -->
 <p><span class="blockTitle">Acreditation</span>
-    <a class="acreditation" href="https://s.id/1Df3c" target="_blank">
-        <img src="https://s.id/1Df4s" alt="Acreditation" width="180" height="70" />
+    <a class="acreditation" href="https://ejournal.uin-suska.ac.id/public/site/images/saide/sinta-4.jpg" target="_blank">
+        <img src="/public/site/images/saide/sinta-4-logo.png" alt="Acreditation" width="180" height="70" />
     </a>
 </p>
 <!-- Right Sidebar - Acreditation -->
